ab4236765d0e50a07c15c475065ba404dfab6c47
Ysrbolles <ysrbolles@gmail.com>
2021-04-05 17:42:06 +0100

    Create Sully  with js

---
+++ JS/Sully.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+
+let i = 5;
+
+(function main(){
+    if(i == 0)  return 0
+    fs.access("Sully_"+i+".js", err =>{
+        if(!err) {i--}
+        fs.writeFile("Sully_"+i+".js", "const fs = require('fs');\nconst { exec } = require('child_process');\n\nlet i = "+i+";\n\n("+main+")()", (err) => {
+            if(!err && i) {
+                exec("node Sully_"+i+".js", (err, stdout, stderr) =>{})
+            }
+        })
+    })
+})()

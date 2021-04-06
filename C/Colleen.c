0304ca8dcf65d7590c92e9bb5f9f149609b01c96
Ysrbolles <ysrbolles@gmail.com>
2021-04-06 15:21:08 +0100

    fix Tabs Err

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,22 +1,22 @@
 /*
 	Comment 1.
 */
-int print(const char *s, ...);
+
 #include <stdio.h>
 
-void Colleen(char const *str)
+void Colleen(const char *str)
 {
-    printf(str, str, 10, 9, 34);
+	printf(str, str, 10, 9, 34);
 }
 
 int main(void)
 {
 
-    /*
+/*
 	Comment 2.
 */
 
-    const char *str = "/*%2$c%3$cComment 1.%2$c*/%2$c%2$c#include <stdio.h>%2$c%2$cvoid Colleen(char const *str)%2$c{%2$c%2$c%3$cprintf(str, str, 10, 9, 34);%2$c}%2$c%2$cint main(void)%2$c{%2$c%2$c/*%2$c%3$cComment 2.%2$c*/%2$c%3$cconst char *str = %4$c%1$s%4$c;%2$c%3$cColleen(str);%2$c%3$creturn (0);%2$c}";
-    Colleen(str);
-    return (0);
+	const char *str = "/*%2$c%3$cComment 1.%2$c*/%2$c%2$c#include <stdio.h>%2$c%2$cvoid Colleen(const char *str)%2$c{%2$c%3$cprintf(str, str, 10, 9, 34);%2$c}%2$c%2$cint main(void)%2$c{%2$c%2$c/*%2$c%3$cComment 2.%2$c*/%2$c%2$c%3$cconst char *str = %4$c%1$s%4$c;%2$c%3$cColleen(str);%2$c%3$creturn (0);%2$c}%2$c";
+	Colleen(str);
+	return (0);
 }
--- C/Sully.c
+++ C/Sully.c
@@ -3,21 +3,26 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define i 6
-#define TOSTR(x) #x
-#define FILE(x) "Sully_" TOSTR(x) ".c"
-#define EXECS(x) "Sully_" TOSTR(x)
-#define PRINT_SULLY(fd, str, i) dprintf(fd, str, str, 10, 9, 34, i);
-#define STR "#include <stdlib.h>%2$c#include <stdio.h>%2$c#include <unistd.h>%2$c#include <fcntl.h>%2$c%2$c#define i %5$d%2$c#define TOSTR(x) #x%2$c#define FILE(x) %4$cSully_%4$cTOSTR(x)%4$c.c%4$c%2$c#define EXECS(x) %4$cSully_%4$c TOSTR(x)%2$c#define PRINT_SULLY(fd, str, i) dprintf(fd, str, str, 10 , 9, 34, i);%2$c%2$c#define STR %4$c%1$s%4$c%2$c%2$c int main(void)%2$c{%2$c%3$cint fd = open(FILE(i), O_WRONLY | O_CREAT, 777);%2$c%3$c if (fd < 0)%2$c%3$c%3$creturn (1);%2$c%3$cPRINT_SULLY(fd, STR, i - 1);%2$c%3$csystem(%4$cgcc %4$cFILE(i) %4$c -o %4$c EXECS(i));%2$c%3$cif(i > 0)system(%4$c./%4$cEXECS(i));%2$c%3$creturn (0);%2$c}%2$c"
+#define STR "#include <stdlib.h>%2$c#include <stdio.h>%2$c#include <unistd.h>%2$c#include <fcntl.h>%2$c%2$c#define STR %4$c%1$s%4$c%2$c%2$cvoid Sully(int fd, char *str, int i)%2$c{%2$c%3$cdprintf(fd, str, str, 10, 9, 34, i);%2$c}%2$c%2$cint main(void)%2$c{%2$c%3$cint i = %5$d;%2$c%3$cchar FILE[10];%2$c%3$cif (!access(%4$cSully_5.c%4$c, F_OK) && i > 0)%2$c%3$c%3$ci--;%2$c%3$csprintf(FILE, %4$cSully_%%d.c%4$c, i);%2$c%3$cint fd = open(FILE, O_RDWR | O_CREAT, 0644);%2$c%3$cif (fd < 0)%2$c%3$c%3$creturn (1);%2$c%3$cSully(fd, STR, i);%2$c%3$cchar se[50];%2$c%3$csprintf(se, %4$cgcc  %%s -o Sully_%%d && ./Sully_%%d%4$c, FILE, i, i);%2$c%3$cif (i > 0) system(se);%2$c%3$creturn (0);%2$c}%2$c"
+
+void Sully(int fd, char *str, int i)
+{
+	dprintf(fd, str, str, 10, 9, 34, i);
+}
 
 int main(void)
 {
-    int fd = open(FILE(i), O_RDWR | O_CREAT, 0644);
-    if (fd < 0)
-        return (1);
-    PRINT_SULLY(fd, STR, i - 1);
-    system("gcc " FILE(i) " -o " EXECS(i));
-    if (i > 0)
-        system("./" EXECS(i));
-    return (0);
+	int i = 5;
+	char FILE[10];
+	if (!access("Sully_5.c", F_OK) && i > 0)
+		i--;
+	sprintf(FILE, "Sully_%d.c", i);
+	int fd = open(FILE, O_RDWR | O_CREAT, 0644);
+	if (fd < 0)
+		return (1);
+	Sully(fd, STR, i);
+	char se[50];
+	sprintf(se, "gcc  %s -o Sully_%d && ./Sully_%d", FILE, i, i);
+	if (i > 0) system(se);
+	return (0);
 }
